ea93dc4abf869b66f6a2874cbe6364e1cd9885f9
Szabina Boli <szabina.boli@skola.sala.se>
2024-03-27 12:41:57 +0100

    Add project files.

---
+++ banansplit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace banansplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
