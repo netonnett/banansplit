883753eb7af08be630a56ffe03ea7366c2380992
Szabina Boli <szabina.boli@skola.sala.se>
2024-03-27 14:35:35 +0100

    heljd

--- banansplit/Program.cs
+++ banansplit/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Intersection
+            string[] intersectionSet1 = { "a", "b", "c", "d", "e" };
+            string[] intersectionSet2 = { "h", "d", "w", "s", "a" };
+            Console.WriteLine("Demonstration av Intersection:");
+            Console.WriteLine("Set 1: " + string.Join(",", intersectionSet1));
+            Console.WriteLine("Set 2: " + string.Join(",", intersectionSet2));
+            List<string> intersectionResult = Intersection(intersectionSet1, intersectionSet2);
+            Console.WriteLine("Resultat: " + string.Join(",", intersectionResult));
+            Console.WriteLine();
+
+        }
+
+        public static List<string> Intersection(string[] set1, string[] set2)
+        {
+            List<string> result = new List<string>();
+            
+            return result;
         }
     }
 }
